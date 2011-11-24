bca8c89cb051dde626b11cc3132f0c1d1c6267b6
max <maksimkabanov@mail.ru>
2011-11-25 00:30:23 +0400

    login

--- application/views/login.php
+++ application/views/login.php
@@ -1,13 +1,98 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Вход</title>
+        <meta http-equiv="Content-script-Type" content="text/html/javascript; charset=utf-8" />
+        <title>Plussia.ru — страна серьезных отношений: Регистрация, шаг 1</title>
+
+        <link rel="stylesheet" type="text/css" href="/css/reset.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/text.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/css/style.css" media="screen" />
+
     </head>
     <body>
-        <form method="POST" action="/login" name="login_form">
-            Логин (e-mail): <input type="text" name="login" value="" />
-            Пароль: <input type="text" name="password" value="" />
-            <button name="send_form">Вход</button>
-        </form>
+        <div id="content">
+
+            <div class="register_head">
+                <div class="margins">
+                    <div class="register_logo"><a href="/"><img src="/images/register/logo.png" /></a></div>			</div><!-- .margins-->
+            </div><!-- .register_head-->
+
+
+
+            <div class="register_main authorize_main">
+                <div class="margins">
+                    <div class="left_column">
+                        <div class="left_column_in">
+
+                            <div class="enter_bg">
+                                <div id="author_form">
+                                    <div class="title">Авторизация</div>
+                                    <form name="login_form" action="/login" method="POST">
+                                        <input class="text" type="text" value="Электронная почта" name="login" />
+                                        <input class="text" type="text" value="Ваш пароль" name="password" />
+
+                                        <div class="p_checkbox active">
+                                            <input type="checkbox" value="remember_me" name="remember_me" checked="checked" />
+				                    Запомнить меня
+                                        </div>
+                                        <span class="lost_pass"><a href="#">Забыли пароль?</a></span>
+
+                                        <input class="send_form" type="image" src="/images/enter_site_ru.png" name="send_form" />
+                                    </form>
+                                </div><!-- #author_form-->
+
+                            </div><!-- .enter_bg-->
+
+                            <div class="author_form_cr">
+							Pararu.ru © 2010 - 2011<br/>
+							Портал Pararu.ru - территория вашего счастья
+                            </div>
+
+
+                        </div><!-- .left_column_in-->
+                    </div><!-- .left_column-->
+
+                    <div class="right_column">
+
+                        <div class="enter_rcol">
+
+                            <p>НЕТ СВОЕГО АККАУНТА?</p>
+
+                            <p>Зарегистрируйтесь и вас ждет:</p>
+                            <ul>
+                                <li>БЕСПЛАНТЫЙ профиль с результатами теста;</li>
+                                <li>Список совместимых с вами спутников;</li>
+                                <li>Полный доступ к системе сравнения;</li>
+                                <li>Консультантации статьи психологов;</li>
+                            </ul>
+
+                            <div id="big_reg_btn"></div>
+                        </div>
+
+                        <a class="author_kasp" href="#"></a>
+
+                    </div><!-- .right_column-->
+
+                </div><!-- .margins-->
+            </div><!-- .main-->
+
+
+            <div class="clear"></div>
+            <div id="empty"></div>
+        </div><!-- #content-->
+
+        <div id="footer">
+
+        </div><!-- #footer-->
+
+        <script type="text/javascript" src="/js/jquery.js"></script>
+        <script type="text/javascript" src="/js/plugins.js"></script>
+        <script type="text/javascript" src="/js/main.js"></script>
+
+
     </body>
+
+
 </html>
+
+
